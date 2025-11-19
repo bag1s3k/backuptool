9117ca22e212e3de8fad074fe218d935b86fcde6
bag1s3k <jetam.mravenec@gmail.com>
2025-11-19 15:05:54 +0100

    Detect wrong value in list according to allowed list
    It works only in test file not with project

--- backuptool/decorator.py
+++ backuptool/decorator.py
@@ -17,23 +17,20 @@ def check_params(allowed: dict): # TODO: option to combinate multiple requiremen
 
             param_to_check = set()
             wrong_types = dict()
-            print(f"\n================ ARGS ==============\n{args}\n")
+            print(args)
             for arg in args:
                 if isinstance(arg, set):
                     param_to_check |= arg
-                if isinstance(arg, dict):
+                elif isinstance(arg, dict):
                     param_to_check |= arg.keys()
                     for arg_key, arg_value in arg.items():
-                        print(f"KEY: {arg_key} VALUE: {arg_value} TYPE: {type(arg_value)}")
+                        # print(f"KEY: {arg_key} VALUE: {arg_value} TYPE: {type(arg_value)}")
                         if isinstance(arg_value, str) and isinstance(allowed[arg_key], list):
                             if arg_value not in allowed[arg_key]:
                                 wrong_types = {arg_key : arg_value}
-                                print(f"wrong_types (archive_type) = {wrong_types}")
-                        # if isinstance(arg_value, list):
-                        #     print(arg_value)
-                        #     if specific_wrongs := [item for item in arg_value if not isinstance(item, str)]:
-                        #         print(f"specific_wrongs {specific_wrongs}")
-                        #         wrong_types |= {arg_key : specific_wrongs}
+                        if isinstance(arg_value, list):
+                            if specific_wrongs := [item for item in arg_value if not isinstance(item, str)]:
+                                wrong_types |= {arg_key : specific_wrongs}
             wrong_keys = set(kwargs.keys() | param_to_check) - set(allowed) - func_params # Set of wrong keys (spelling)
             if wrong_keys:
                 raise ValueError(f"Unknown params: {[*wrong_keys]}. Allowed: {allowed}")
