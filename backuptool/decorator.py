27795eca70e4b38bc0ab5e3d130c11e41a216389
bag1s3k <jetam.mravenec@gmail.com>
2025-11-19 10:22:58 +0100

    Detect wrong value according to the allowed values
    First version, ugly code, generally it only works

--- backuptool/decorator.py
+++ backuptool/decorator.py
@@ -2,7 +2,7 @@
 from functools import wraps
 
 
-def check_kwargs(*allowed_params: set):
+def check_params(allowed: dict): # TODO: option to combinate multiple requirements
 
     def decorator(func):
         @wraps(func)
@@ -11,19 +11,41 @@ def check_kwargs(*allowed_params: set):
                 :param kwargs: variable to check
                 """
 
-            # Get parameters
+            # Get parameter names (excluding **kwargs)
             sig = inspect.signature(func)
             func_params = set(sig.parameters.keys())
 
-            # If arg equals to set that means it's another params, add to review
             param_to_check = set()
+            wrong_types = dict()
+            print(f"\n================ ARGS ==============\n{args}\n")
             for arg in args:
                 if isinstance(arg, set):
                     param_to_check |= arg
-
-            if unknown_params := set(kwargs.keys() | param_to_check) - set(*allowed_params) - func_params:
-                raise ValueError(f"Unknown params: {[*unknown_params]}. Allowed: {[*allowed_params]}")
+                if isinstance(arg, dict):
+                    param_to_check |= arg.keys()
+                    for arg_key, arg_value in arg.items():
+                        print(f"KEY: {arg_key} VALUE: {arg_value} TYPE: {type(arg_value)}")
+                        if isinstance(arg_value, str) and isinstance(allowed[arg_key], list):
+                            if arg_value not in allowed[arg_key]:
+                                wrong_types = {arg_key : arg_value}
+                                print(f"wrong_types (archive_type) = {wrong_types}")
+                        # if isinstance(arg_value, list):
+                        #     print(arg_value)
+                        #     if specific_wrongs := [item for item in arg_value if not isinstance(item, str)]:
+                        #         print(f"specific_wrongs {specific_wrongs}")
+                        #         wrong_types |= {arg_key : specific_wrongs}
+            wrong_keys = set(kwargs.keys() | param_to_check) - set(allowed) - func_params # Set of wrong keys (spelling)
+            if wrong_keys:
+                raise ValueError(f"Unknown params: {[*wrong_keys]}. Allowed: {allowed}")
+            elif wrong_types:
+                tmp = wrong_types.keys() & allowed.keys()
+                raise ValueError(f"Invalid data types: {" ".join(f"{k}={v}" for k,v in wrong_types.items())}. Allowed: {" ".join(f"{k}={v}" for k,v in allowed.items() if k in tmp)}")
 
             return func(*args, **kwargs)
         return wrapper
     return decorator
+
+# CONTINUE
+# - discovery that list of allowed value e.g ["zip", "tar"] is never used in list it's always 1 string
+# - list (that means [str])
+# - value (that means ["zip", "tar"]) values have to exactly match with that's in list
